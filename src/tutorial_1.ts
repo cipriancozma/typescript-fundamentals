5f1b9991c5c00b163a19d97ba419e4668c5c8aaf
Ciprian <cozma_ciprian19@yahoo.com>
2024-03-25 17:44:37 +0200

    added new info on the fundamentals of ts

--- src/tutorial_1.ts
+++ src/tutorial_1.ts
@@ -12,3 +12,68 @@ let tax: number | string = '10'
 let requestStatus: 'pending' | 'success' | 'error' = 'pending'
 
 // 4.  Type Any
+let notSure: any = 4;
+notSure = false
+
+// Practical Example
+const books = ['1984', 'Brave World', 'Fahrenheit']
+
+let foundBook: string | undefined;
+
+for(let book of books) {
+    if (book === '1984') {
+        foundBook = book;
+        break;
+    }
+}
+
+// 5. Arrays
+let prices: number[] = [20, 19, 30]
+let arr: (string | number)[] = [1, 2, 3, "four", 'five']
+
+// 6. Objects
+type Car = {
+    brand: string,
+    year: number
+}
+
+type Items = {
+    title: string,
+    cost?: number
+}
+
+let car: Car = {
+    brand: "BMW",
+    year: 2020
+}
+
+let items: Items[] = [
+    {
+        title: "Book1",
+        cost: 100
+    },
+    {
+        title: "Book2",
+        // cost: 150
+    }
+]
+
+// 7. Functions
+function sayHi(name: string): string {
+    return name
+}
+const arr1: string[] = ['Ciprian', "Cosmin", 'Andrei']
+function checkName(name: string): boolean {
+    return arr1.includes(name)
+}
+
+// checkName('Ciprian')
+
+// 7.1. Functions - Optional Params
+function calculatePrice(price: number, discount?: number): number {
+    return price - (discount || 0)
+}
+
+let priceAfterDiscount = calculatePrice(100, 20)
+
+// 7.2. Functions - Default Parameter
