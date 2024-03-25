3fec5f43df7ac6dc31cf778ca2e16ef1d1d4d797
Ciprian <cozma_ciprian19@yahoo.com>
2024-03-25 10:00:56 +0200

    added new info to tutorial_1

--- src/tutorial_1.ts
+++ src/tutorial_1.ts
@@ -5,5 +5,10 @@ let isMarried: boolean = true
 let amount: number = 19
 
 // 2. Type Inference
+// TypeScript infers the type on a variable, if you don't specify it in the variable.
 
+// 3. Union Type
+let tax: number | string = '10'
+let requestStatus: 'pending' | 'success' | 'error' = 'pending'
 
+// 4.  Type Any
